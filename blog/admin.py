37eca2f82d2aa6946c246b15b439f0eb632bf9cf
Ken Atopos <z1158229761@outlook.com>
2023-03-29 11:57:07 +0100

    add session

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tag, Author, Post
+from .models import Tag, Author, Post, Comment
 
 # Register your models here.
 
@@ -12,6 +12,11 @@ class PostAdmin(admin.ModelAdmin):
     }
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
